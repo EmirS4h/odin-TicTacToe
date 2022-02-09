242b14861bfbd370bf622bad6b41ba3c8f0fbeca
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-02-09 12:14:23 +0300

    Updated ui added more features

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const boardContainer = document.querySelector(".boardContainer");
 const winnerText = document.querySelector("#winnerText");
+
 const gameBoard = (() => {
   let board = [
     ["", "", ""],
@@ -26,14 +27,28 @@ const game = ((gameBoard) => {
       : (currentPlayer = player1);
   };
 
+  const checkForDraw = () => {
+    for (let i = 0; i < gameBoard.board.length; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (gameBoard.board.every((row) => row.every((el) => el !== ""))) {
+          isGameOver = true;
+          highlightWinningPatternRow();
+          return { name: "draw" };
+        }
+      }
+    }
+  };
+
   const checkRowsForWin = () => {
     for (let i = 0; i < gameBoard.board.length; i++) {
       for (let j = 0; j < 3; j++) {
         if (gameBoard.board[i].every((el) => el === "X")) {
           isGameOver = true;
+          highlightWinningPatternRow(i);
           return player1;
         } else if (gameBoard.board[i].every((el) => el === "O")) {
           isGameOver = true;
+          highlightWinningPatternRow(i);
           return player2;
         }
       }
@@ -41,8 +56,10 @@ const game = ((gameBoard) => {
   };
 
   const checkForWinner = () => {
-    let result = checkRowsForWin();
-    winnerText.textContent = result ? `${result.name} Wins!` : "";
+    let result = checkRowsForWin() || checkForDraw();
+    winnerText.textContent = result
+      ? `${result.name === "draw" ? "DRAW" : result.name + " Wins!"}`
+      : "";
   };
 
   const handleClick = (boardItem, row, col) => {
@@ -60,6 +77,51 @@ const game = ((gameBoard) => {
     boardContainer.appendChild(boardItem);
   };
 
+  const clearBoard = () => {
+    boardContainer.textContent = "";
+    gameBoard.board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+  };
+
+  const highlight = (arr, bgColor) => {
+    for (let node of arr) {
+      node.style.backgroundColor = bgColor;
+      node.style.color = "white";
+    }
+  };
+
+  const highlightWinningPatternRow = (row) => {
+    let nodes = Array.from(boardContainer.childNodes);
+    switch (row) {
+      case 0:
+        nodes = nodes.slice(0, 3);
+        highlight(nodes, "rgb(25, 167, 77)");
+        break;
+      case 1:
+        nodes = nodes.slice(3, 6);
+        highlight(nodes, "rgb(25, 167, 77)");
+        break;
+      case 2:
+        nodes = nodes.slice(6, 9);
+        highlight(nodes, "rgb(25, 167, 77)");
+        break;
+      default:
+        highlight(nodes, "rgb(191, 31, 31)");
+        break;
+    }
+  };
+
+  const restart = () => {
+    currentPlayer = player1;
+    isGameOver = false;
+    winnerText.textContent = "";
+    clearBoard();
+    render();
+  };
+
   const render = () => {
     for (let i = 0; i < gameBoard.board.length; i++) {
       for (let j = 0; j < gameBoard.board[i].length; j++) {
@@ -68,7 +130,12 @@ const game = ((gameBoard) => {
     }
   };
 
-  return { render };
+  return { render, restart };
 })(gameBoard);
 
 game.render();
+
+document.addEventListener("keydown", (e) => {
+  e.preventDefault();
+  if (e.key === "r") game.restart();
+});
